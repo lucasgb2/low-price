59117a52c20b3264e268aefa7c2f3bc44610da47
Lucas Garcia Batista <lucasgb2@gmail.com>
2023-06-16 08:44:33 -0300

    - Ajust deploy to Heroku

--- backend/src/service/scrapyproduct.py
+++ backend/src/service/scrapyproduct.py
@@ -1,7 +1,9 @@
 import asyncio
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions, FirefoxOptions
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
 import os
 
@@ -22,18 +24,37 @@ class ScrapyProduct:
     def getDriver(self):
         try:
             driver = None
-            drivername: str = 'firefox'
+            driver_name: str = 'chrome'
             if os.environ.get('DRIVERSCRAPY') is not None:
-                drivername = os.environ['DRIVERSCRAPY']
+                driver_name = os.environ['DRIVERSCRAPY']
 
-            if drivername == 'firefox':
-                fireFoxOptions: FirefoxOptions = webdriver.FirefoxOptions()
-                fireFoxOptions.add_argument('--headless')
-                path_geckodriver = os.path.join(os.path.dirname(__file__), 'geckodriver')
-                driver = webdriver.Firefox(options=fireFoxOptions, executable_path=path_geckodriver)
+            if driver_name == 'firefox':
+                firefox_options: FirefoxOptions = webdriver.FirefoxOptions()
+                firefox_options.add_argument('-headless')
+                firefox_options.add_argument("-disable-gpu")
+                firefox_options.add_argument("-no-sandbox")
+                if os.environ.get("FIREFOX_BIN") is not None:
+                    firefox_options.binary_location = os.environ.get("FIREFOX_BIN")
+
+                executable_path = 'geckodriver'
+                if os.environ.get("FIREFOXDRIVER_PATH") is not None:
+                    executable_path = os.environ.get("FIREFOXDRIVER_PATH")
+
+                driver = webdriver.Firefox(options=firefox_options, executable_path=executable_path)
             else:
-                #chromeOptions: ChromeOptions = webdriver.ChromeOptions()
-                driver = webdriver.Chrome()
+                chrome_options: ChromeOptions = webdriver.ChromeOptions()
+                chrome_options.add_argument('--headless')
+                chrome_options.add_argument("--disable-dev-shm-usage")
+                chrome_options.add_argument("--no-sandbox")
+
+                if os.environ.get("GOOGLE_CHROME_BIN") is not None:
+                    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+                executable_path = 'chromedriver'
+                if os.environ.get("CHROMEDRIVER_PATH") is not None:
+                    executable_path = os.environ.get("CHROMEDRIVER_PATH")
+
+                driver = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options)
 
             return driver
         except NameError:
@@ -45,6 +66,7 @@ class ScrapyProduct:
         try:
             driver = self.getDriver()
             driver.get(self.get_url())
+            elem = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, 'product_description')))
             elem = driver.find_element(By.ID, 'product_description')
             if elem:
                 self.description = elem.text
